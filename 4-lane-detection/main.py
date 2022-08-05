674d92cc82df1eac8008c6baa384876f0200a4f0
ashwinpra <ashprasanth2003@gmail.com>
2022-08-05 19:00:02 +0530

    save processed videos

--- 4-lane-detection/main.py
+++ 4-lane-detection/main.py
@@ -1,5 +1,44 @@
-import lanedetection
+import cv2 
+import numpy as np
+from functions import * 
 
 level = int(input("Enter level: "))
+
 vid = cv2.VideoCapture(f"./raw-videos/level{level}.mp4")
-#out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
+   
+lower_y = np.array([20,100,100],dtype=np.uint8)
+upper_y = np.array([40,255,255],dtype=np.uint8)
+lower_w = 220
+upper_w = 255
+
+last_left_line = None
+last_right_line = None
+
+last_y_lines = None
+last_w_lines = None
+
+while 1:
+    ret, frame = vid.read() 
+
+    if ret:
+        y_processed, w_processed, all_lines = processYWLines(frame,lower_y,upper_y,lower_w,upper_w)
+        try:
+            left_line,right_line = getLR(all_lines)
+        except:
+            continue
+
+        try:
+            frame = drawBox(frame,left_line,right_line)
+        except:
+            continue
+
+        cv2.imshow("Lane Detection",frame)
+
+    else:
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == ord('0'): 
+        break
+
+vid.release()
+cv2.destroyAllWindows()
